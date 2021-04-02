570bc93d549c9afee150904864252fbc245f8269
TaufeqRazakh <taufeqmdr@gmail.com>
2021-04-02 13:35:14 -0700

    sbatch script disvocey fixes

--- sbatch_PND.sh
+++ sbatch_PND.sh
@@ -4,4 +4,10 @@
 #SBATCH --time=30
 #SBATCH --output="PND-sbatch-test-%A.out"
 
+tar -cvzf PND.tgz PND/
+
+[ -f "build_run_PND_Discovery.sh" ]  && rm build_run_PND_Discovery.sh
+
+cp PND/build_run_PND_Discovery.sh .
+
 sh build_run_PND_Discovery.sh

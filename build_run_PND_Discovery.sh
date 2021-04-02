cb99bf1e7ab33684088e9a9b46f826c06370a4df
TaufeqRazakh <taufeqmdr@gmail.com>
2021-04-02 13:38:43 -0700

    build script discovery

---
+++ build_run_PND_Discovery.sh
@@ -0,0 +1,26 @@
+module purge
+module load usc
+module load cuda/10.1.243 python/3.7.6 cmake/3.16.2 cudnn/8.0.2-10.1
+
+cds2
+
+[ -d "run_PND" ] && { echo "Purging past source code from scratch"; rm -rf run_PND }
+
+mkdir run_PND; cd run_PND
+
+cp ~/PND.tgz ./
+
+tar -xzvf PND.tgz 
+
+cd PND/
+
+mkdir build; cd build
+
+python3 -c 'import torch ; print(torch.utils.cmake_prefix_path)'
+
+CC=gcc CXX=g++ cmake -DCMAKE_PREFIX_PATH='/spack/apps/linux-centos7-x86_64/gcc-8.3.0/python-3.7.6-dd2am3dyvlpovhd4rizwfzc45wnsajxf/lib/python3.7/site-packages/torch/share/cmake;/usr/lib64' ../
+
+cmake --build . --config Release
+
+./pnd_example
+

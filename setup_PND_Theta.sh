c21d6f8fbd9115d0237c1496c61e71794d5ecca9
TaufeqRazakh <taufeqmdr@gmail.com>
2021-04-02 04:17:19 -0700

    TODO: request KNl nodes. build with CMake and PyTorch

--- setup_PND_Theta.sh
+++ setup_PND_Theta.sh
@@ -1,26 +1,18 @@
-module purge
-module load usc
-module load cuda/10.1.243 python/3.7.6 cmake/3.16.2 cudnn/8.0.2-10.1
+#!/bin/sh
+#COBALT -n 1 -t 0:30:00
+#COBALT -q debug-cache-quad --attrs mcdram=cache:numa=quad
+#COBALT -A UltrafastMat
+#COBALT -o Pytorch_CMake_Message.out
 
-cd /scratch2/razakh/
+module load datascience/pytorch-0.5.0-mkldnn datascience/horovod-0.13.11
 
-[ -d "run_PND" ] && echo "Purging past source code from scratch" rm -rf run_PND
-
-mkdir run_PND; cd run_PND
-
-cp ~/PND.tgz ./
-
-tar -xzvf PND.tgz 
-
-cd PND/
+NPROC_PER_NODE=4
+NPROC=$((NPROC_PER_NODE*COBALT_JOBSIZE))
 
 mkdir build; cd build
 
-python3 -c 'import torch ; print(torch.utils.cmake_prefix_path)'
+python3 -c 'import hotovod.torch as hvd; print(hvd.torch.utils.cmake_prefix_path)'
 
-CC=gcc CXX=g++ cmake -DCMAKE_PREFIX_PATH='/spack/apps/linux-centos7-x86_64/gcc-8.3.0/python-3.7.6-dd2am3dyvlpovhd4rizwfzc45wnsajxf/lib/python3.7/site-packages/torch/share/cmake;/usr/lib64' ../
+CC=gcc CXX=g++ cmake -DCMAKE_PREFIX_PATH='python3 -c 'import hotovod.torch as hvd; print(hvd.torch.utils.cmake_prefix_path)'' ../
 
 cmake --build . --config Release
-
-./pnd_example
-
